710ba6c0406f848e1d856f963cf308cf728f12e6
Jorge Juan Barroso Carmona <jorge@karumi.com>
2019-05-09 12:04:27 +0200

    Add the first unit test, with when infraestructure to invoke methods.

---
+++ tests/helloWorld.spec.js
@@ -0,0 +1,10 @@
+const when = require("./steps/when")
+
+describe(`When we invoke the GET /helloWorld endpoint`, () => {
+    test(`Should return the right greeting`, async () => {
+        const name = "Manolito"
+        response = await when.we_invoke_helloWorld(name);
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toBe("Hello Manolito");
+    });
+});
---
+++ tests/steps/when.js
@@ -0,0 +1,11 @@
+module.exports.we_invoke_helloWorld = (name) => {
+    const event = { pathParameters: { name: name } };
+    return viaHandler("helloWorld", event);
+}
+
+async function viaHandler(functionPath, event) {
+    const handler = require(`../../src/functions/${functionPath}`);
+    const response = await handler.handler(event);
+    response.body = JSON.parse(response.body);
+    return response;
+}
