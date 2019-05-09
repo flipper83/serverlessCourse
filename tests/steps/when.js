3359f51777d4458ad39d3332daf600f6d73d7bd3
Jorge Juan Barroso Carmona <jorge@karumi.com>
2019-05-09 12:27:03 +0200

    Added first acceptance tests using superagent and invoke one or other depends of the when code.

--- tests/steps/when.js
+++ tests/steps/when.js
@@ -1,9 +1,30 @@
 module.exports.we_invoke_helloWorld = (name) => {
-    const event = { pathParameters: { name: name } };
-    return viaHandler("helloWorld", event);
+    const mode = process.env.TEST_MODE;
+    return mode == "http" ? viaHttp("helloWorld", name) : viaHandler("helloWorld", name);
 }
 
-async function viaHandler(functionPath, event) {
+async function viaHttp(functionPath, name) {
+    const http = require("superagent-promise")(require("superagent"), Promise);
+    const host = `https://f4s25sgz8h.execute-api.eu-west-1.amazonaws.com/dev/api/${functionPath}`
+    const path = `${host}/${name}`
+    
+    try {
+        const httpReq = http("GET", path);
+        const res = await httpReq;
+        
+        return {
+            statusCode: res.status,
+            body: res.body
+        }
+    }catch (err) {
+        return {
+            statusCode: err.statu
+        }
+    }
+}
+
+async function viaHandler(functionPath, name) {
+    const event = { pathParameters: { name: name } };
     const handler = require(`../../src/functions/${functionPath}`);
     const response = await handler.handler(event);
     response.body = JSON.parse(response.body);
