28d01df19fe4c6603128848b4d087077ff3f6707
Jorge Juan Barroso Carmona <jorge@karumi.com>
2019-05-09 17:32:50 +0200

    subscribe to a sns topic and print the values in the method notify to the organizer.

---
+++ src/functions/notifyOrganizer.js
@@ -0,0 +1,7 @@
+exports.handler = async (event, context) => {
+    const orderPlaced = JSON.parse(event.Records[0].Sns.Message);
+  
+    console.log(`notified organiser [${orderPlaced.getTogetherId}, ${orderPlaced.orderId}, ${orderPlaced.userEmail}]`);
+    
+    return orderPlaced.userEmail;
+};
