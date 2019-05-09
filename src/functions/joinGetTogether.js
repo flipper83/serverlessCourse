6c54a5a287a2a7ea555e29481d57504d828368b1
Jorge Juan Barroso Carmona <jorge@karumi.com>
2019-05-09 17:52:36 +0200

    publish the message to the queue using and post api.

---
+++ src/functions/joinGetTogether.js
@@ -0,0 +1,35 @@
+const AWS = require("aws-sdk");
+const chance = require("chance").Chance();
+const sns = new AWS.SNS();
+
+exports.handler = async (event, context) => {
+
+    const body = JSON.parse(event.body);
+    const getTogetherId = body.getTogetherId;
+    const userEmail = body.getTogetherEmail;
+
+    const orderId = chance.guid();
+    console.log(`user ${userEmail} joining gettogether ${getTogetherId}`);
+
+    const data = {
+        orderId,
+        getTogetherId,
+        userEmail
+    };
+
+    const params = {
+        Message: JSON.stringify(data),
+        TopicArn: process.env.joinGetTogetherSnsTopic
+    };
+
+    await sns.publish(params).promise();
+
+    console.log("published 'join_getTogether' event");
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({ orderId })
+    };
+
+    return response;
+};
